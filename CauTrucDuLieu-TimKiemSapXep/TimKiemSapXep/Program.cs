dd7458327873b0b6e3ffc20bc6aea8ccb7dfe111
nguyentu1112005 <nguyentu1112005@gmail.com>
2024-01-26 18:00:35 +0700

    code hoan thien

--- CauTrucDuLieu-TimKiemSapXep/TimKiemSapXep/Program.cs
+++ CauTrucDuLieu-TimKiemSapXep/TimKiemSapXep/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,18 +19,18 @@ namespace TimKiemSapXep
             Console.WriteLine(" Mang ban dau duoc phat sinh ngau nhien : ");
             XuatMang(a, n);
 
-            SapXepTang(a, n);
-            Console.WriteLine("\n Mang duoc sap cep tang : ");
+            QuickSort(a, 0, n-1);
+            Console.WriteLine("\n Mang duoc sap xep tang : ");
             XuatMang(a, n);
 
-            Console.Write("\n Nhap gia tri x can tim : ");
-            int x = int.Parse(Console.ReadLine());
+            //Console.Write("\n Nhap gia tri x can tim : ");
+            //int x = int.Parse(Console.ReadLine());
 
-            int kq = TimNhiPhan(a, n, x);
-            if (kq == -1)
-                Console.WriteLine($"{x} khong xuat hien trong mang ");
-            else
-                Console.WriteLine($"{x} xuat hien trong mang tai vi tri : {kq} ");
+            //int kq = TimNhiPhan(a, n, x);
+            //if (kq == -1)
+            //    Console.WriteLine($"{x} khong xuat hien trong mang ");
+            //else
+            //    Console.WriteLine($"{x} xuat hien trong mang tai vi tri : {kq} ");
 
             Console.ReadLine();
         }
@@ -91,5 +92,84 @@ namespace TimKiemSapXep
                 }
             }
         }
+        static void SelectionSort(int[] a, int n)
+        {
+            int tam, dem = 0;
+            for(int i = 0;i < n - 1; i++)
+            {
+                int min = i;
+                for(int j = i + 1;j < n; j++)
+                {
+                    if (a[min] > a[j])
+                        min = j;
+                }
+                if(min !=  i)
+                {
+                    tam = a[i];
+                    a[i] = a[min];
+                    a[min] = tam;
+                    dem++;
+                }
+                Console.Write($"\nLan lap i = {i} : ");
+                XuatMang(a, n);
+            }
+        }
+        static void InsertionSort(int[] a, int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                int x = a[i];
+                int j = i;
+                while (j > 0 && a[j - 1] > x)
+                {
+                    a[j] = a[j - 1];
+                    j--;
+                }
+                a[j] = x;
+            }
+        }
+        static void BubbleSort(int[] a, int n)
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    if (a[j - 1] > a[j])
+                    {
+                        int tam = a[j];
+                        a[j] = a[j - 1];
+                        a[j - 1]= tam;
+                    }
+                }
+            }
+        }
+        static void QuickSort(int[] a, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int x = a[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (a[i] < x) i++;
+                while (a[j] > x) j--;
+
+                if (i <= j)
+                {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+                QuickSort(a, left, j);
+            if (i < right)
+                QuickSort(a, i, right);
+        }
+
     }
 }
